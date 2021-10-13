3a1a00ff9131b4b0091ec0f5c965b0c5d1f5399d
RSWilli <bartel.wilhelm@gmail.com>
2021-10-13 23:04:56 +0200

    use clrf as per spec

--- lib/makeCalendar.ts
+++ lib/makeCalendar.ts
@@ -2,24 +2,28 @@ import dayjs from "dayjs"
 import { CalEvent, Events } from "./helper"
 
 export const makeEvent = (event: CalEvent) => {
-    return `BEGIN:VEVENT
-DTSTAMP:${dayjs().format("YYYYMMDDTHHmmss")}
-DTSTART;TZID=Europe/Berlin:${event.date.format("YYYYMMDDTHHmmss")}
-DURATION:PT45M
-SUMMARY:${event.subject}
-DESCRIPTION: ${event.presenter}
-LOCATION:${event.room}
-UID:blindow_${event.date.unix()}
-END:VEVENT`
+    return [
+        `BEGIN:VEVENT`,
+        `DTSTAMP:${dayjs().format("YYYYMMDDTHHmmss")}`,
+        `DTSTART;TZID=Europe/Berlin:${event.date.format("YYYYMMDDTHHmmss")}`,
+        `DURATION:PT45M`,
+        `SUMMARY:${event.subject}`,
+        `DESCRIPTION: ${event.presenter}`,
+        `LOCATION:${event.room}`,
+        `UID:blindow_${event.date.unix()}`,
+        `END:VEVENT`
+    ].join("\r\n")
 }
 export const makeCalendar = (events: Events) => {
 
-    return `BEGIN:VCALENDAR
-X-WR-CALDESC:Blindow
-X-WR-TIMEZONE:Europe/Berlin
-CALSCALE:GREGORIAN
-VERSION:2.0
-${events.map(makeEvent).join("\n")}
-END:VCALENDAR
-`
+    return [
+        `BEGIN:VCALENDAR`,
+        `X-WR-CALDESC:Blindow`,
+        `X-WR-TIMEZONE:Europe/Berlin`,
+        `CALSCALE:GREGORIAN`,
+        `VERSION:2.0`,
+        `${events.map(makeEvent).join("\r\n")}`,
+        `END:VCALENDAR`
+    ].join("\r\n")
+
 }
