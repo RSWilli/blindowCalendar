832fdcb53a31a7e0862ed9c2ee5a61119afa7a81
RSWilli <bartel.wilhelm@gmail.com>
2021-10-13 23:13:45 +0200

    complying to ics standard

--- lib/makeCalendar.ts
+++ lib/makeCalendar.ts
@@ -8,7 +8,7 @@ export const makeEvent = (event: CalEvent) => {
         `DTSTART;TZID=Europe/Berlin:${event.date.format("YYYYMMDDTHHmmss")}`,
         `DURATION:PT45M`,
         `SUMMARY:${event.subject}`,
-        `DESCRIPTION: ${event.presenter}`,
+        `DESCRIPTION:${event.presenter}`,
         `LOCATION:${event.room}`,
         `UID:blindow_${event.date.unix()}`,
         `END:VEVENT`
@@ -18,11 +18,30 @@ export const makeCalendar = (events: Events) => {
 
     return [
         `BEGIN:VCALENDAR`,
-        `X-WR-CALDESC:Blindow`,
+        `X-WR-CALDESC:blindow`,
+        `X-WR-CALNAME:blindow`,
         `X-WR-TIMEZONE:Europe/Berlin`,
         `CALSCALE:GREGORIAN`,
+        `PRODID:blindow-parser`,
         `VERSION:2.0`,
         `${events.map(makeEvent).join("\r\n")}`,
+        `BEGIN:VTIMEZONE`,
+        `TZID:Europe/Berlin`,
+        `BEGIN:STANDARD`,
+        `DTSTART:19701025T030000`,
+        `RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU`,
+        `TZNAME:CET`,
+        `TZOFFSETFROM:+0200`,
+        `TZOFFSETTO:+0100`,
+        `END:STANDARD`,
+        `BEGIN:DAYLIGHT`,
+        `DTSTART:19700329T020000`,
+        `RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU`,
+        `TZNAME:CEST`,
+        `TZOFFSETFROM:+0100`,
+        `TZOFFSETTO:+0200`,
+        `END:DAYLIGHT`,
+        `END:VTIMEZONE`,
         `END:VCALENDAR`
     ].join("\r\n")
 
